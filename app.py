9aa84b4d090a27af89fd943c3cd783aa157fc97b
pvihar <pvh12115@gmail.com>
2020-12-03 16:37:39 +0530

    Added details in files add-subject, add-faculty, add-classroom etc.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-vapp = Flask(__name__)
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy(app)
 
