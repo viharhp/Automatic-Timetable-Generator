717b350dbf019d3ee3d97d16b892d4db21c465d7
pvihar <pvh12115@gmail.com>
2020-12-08 00:45:15 +0530

    Connected all the databases to tables, added javascript to add multiple drop-down menus.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class Subject(db.Model):
         return str(self.id)
 
 class Faculty(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    faculty_id = db.Column(db.Integer, primary_key=True)
     faculty_name = db.Column(db.String(40), nullable=False)
     faculty_hours = db.Column(db.Integer, nullable=False)
     faculty_subject1 = db.Column(db.String(40), default='N/A')
@@ -29,17 +29,15 @@ class Faculty(db.Model):
     faculty_subject7 = db.Column(db.String(40), default='N/A')
 
     def __repr__(self):
-        # returns id as default name of row
-        return str("Subject" + self.id)
+        return str(self.faculty_id)
 
 class Classroom(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    class_id = db.Column(db.Integer, primary_key=True)
     class_type = db.Column(db.String(40), nullable=False)
     class_number = db.Column(db.String(40), nullable=False)
 
     def __repr__(self):
-        # returns id as default name of row
-        return str("Subject" + self.id)
+        return str(self.class_id)
 
 @app.route('/')
 def index():
@@ -61,15 +59,35 @@ def add_sub():
         db.session.commit() # added permanently
         return redirect('/add-subject')
     else:
-        return render_template('add-subject.html')
+        all_subjects = Subject.query.order_by(Subject.id).all()
+        return render_template('add-subject.html', subjects = all_subjects)
+        # defined subjects variable which stores all_subjects, which we will use in drop down menu
 
-@app.route('/add-classroom')
+@app.route('/add-classroom', methods=['GET','POST'])
 def add_classroom():
+    if request.method == "POST":
+        class_type = request.form.get('classroomtype')
+        class_number = request.form['class_number']
+        new_class = Classroom(class_type=class_type, class_number=class_number)
+        db.session.add(new_class) # added for this session only
+        db.session.commit() # added permanently
+        return redirect('/add-classroom')
+    else:
         return render_template('add-classroom.html')
 
-@app.route('/add-faculty')
+@app.route('/add-faculty', methods=['GET','POST'])
 def add_faculty():
-        return render_template('add-faculty.html')
+    if request.method == "POST":
+        faculty_name = request.form['faculty_name']
+        faculty_hours = request.form['faculty_hours']
+        faculty_subject = request.form.get('faculty_subject')
+        new_faculty = Faculty(faculty_name=faculty_name, faculty_hours=faculty_hours, faculty_subject1=faculty_subject)
+        db.session.add(new_faculty) # added for this session only
+        db.session.commit() # added permanently
+        return redirect('/add-faculty')
+    else:
+        all_subjects = Subject.query.order_by(Subject.id).all()
+        return render_template('add-faculty.html', subjects = all_subjects)
 
 @app.route('/generate')
 def generate():
